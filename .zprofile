210730ac7669e8092e7b9057762dd84d5d39ec29
speg03 <speg03@gmail.com>
2012-10-17 17:34:51 +0900

    zsh, screenの設定ファイルを追加

---
+++ .zprofile
@@ -0,0 +1,1 @@
+export PATH=$HOME/local/bin:$HOME/bin:/usr/local/bin:/bin:/usr/bin:/usr/local/sbin:/usr/sbin:/sbin
--- setup.sh
+++ setup.sh
@@ -3,10 +3,16 @@
 fullpath=`readlink -e $0`
 basedir=`dirname $fullpath`
 
-## Emacsの設定
+## 各設定の有効化
 cd $basedir
-ln -sf `readlink -e .emacs.d` $HOME
+ln -sf \
+  `readlink -e .emacs.d` \
+  `readlink -e .zshrc` \
+  `readlink -e .zprofile` \
+  `readlink -e .screenrc` \
+  $HOME
 
+## Emacsの設定
 cd .emacs.d/elisp
 curl -LO http://www.emacswiki.org/emacs/download/auto-install.el
 curl -LO https://gist.github.com/raw/1021706/b9aff51e7e40afa7abb3c7d6ef7708993ad93b04/init-loader.el
