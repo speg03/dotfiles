7f12ca7e315f5b89caa0a026f1f9e8d42f41e9ab
speg03 <speg03@gmail.com>
2012-10-25 11:03:38 +0900

    howmのインストール手順を追加

--- setup.sh
+++ setup.sh
@@ -14,6 +14,14 @@ ln -sf \
   $HOME
 
 ## Emacsの設定
-cd .emacs.d/elisp
+cd $basedir/.emacs.d/elisp
 curl -LO http://www.emacswiki.org/emacs/download/auto-install.el
 curl -LO https://gist.github.com/raw/1021706/b9aff51e7e40afa7abb3c7d6ef7708993ad93b04/init-loader.el
+
+cd $basedir/.emacs.d/archives
+curl -LO http://howm.sourceforge.jp/a/howm-1.4.0.tar.gz
+tar xzf howm-1.4.0.tar.gz
+cd howm-1.4.0
+./configure
+make
+sudo make install
