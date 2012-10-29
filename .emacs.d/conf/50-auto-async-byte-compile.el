d494e941c3440ede8767455535d391547bcb5d88
speg03 <speg03@gmail.com>
2012-10-29 10:08:09 +0900

    elispを保存時に自動コンパイルする

---
+++ .emacs.d/conf/50-auto-async-byte-compile.el
@@ -0,0 +1,3 @@
+(when (require 'auto-async-byte-compile nil t)
+  (setq auto-async-byte-compile-exclude-files-regexp "/junk/")
+  (add-hook 'emacs-lisp-mode-hook 'enable-auto-async-byte-compile-mode))
