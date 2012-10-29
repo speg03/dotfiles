5ad42c00eb85117209f32f921636086076cd846d
speg03 <speg03@gmail.com>
2012-10-29 10:26:31 +0900

    elispの関連URL追記

--- .emacs.d/conf/50-auto-async-byte-compile.el
+++ .emacs.d/conf/50-auto-async-byte-compile.el
@@ -1,3 +1,8 @@
+;;; -*- coding: utf-8; mode: emacs-lisp; -*-
+
+;; M-x install-elisp-from-emacswiki auto-async-byte-compile.el
+;; http://d.hatena.ne.jp/rubikitch/20100423/bytecomp
+
 (when (require 'auto-async-byte-compile nil t)
   (setq auto-async-byte-compile-exclude-files-regexp "/junk/")
   (add-hook 'emacs-lisp-mode-hook 'enable-auto-async-byte-compile-mode))
