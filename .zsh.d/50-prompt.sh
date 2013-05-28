52d7c65d57926733e465cfea3854d7b1df0ad1ef
speg03 <speg03@gmail.com>
2013-05-28 17:50:50 +0900

    プロンプト部分にマルチバイト文字を使わないように変更。

--- .zsh.d/50-prompt.sh
+++ .zsh.d/50-prompt.sh
@@ -1,6 +1,6 @@
-# ┌(23:59:59) user@hostname [~/path]
-# └>$ 
+# ┌>(23:59:59) user@hostname [~/path]
+# $ 
 
-p1mark='┌'
-p2mark='└'
-PROMPT=$'$p1mark%{$fg[cyan]%}(%*)%{$reset_color%} %{$fg[green]%}%n@%m%{$reset_color%} %{$fg[yellow]%}[%2~]%{$reset_color%}\n$p2mark>%(!|%{$fg[red]%}#%{$reset_color%}|$) '
+p1mark='┌>'
+p2mark=''
+PROMPT=$'$p1mark%F$fg[cyan](%*)%f %F$fg[green]%n@%m%f %F$fg[yellow][%2~]%f\n$p2mark%(!|%F%{$fg[red]%}#%f|$) '
