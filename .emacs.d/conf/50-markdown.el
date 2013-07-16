8e5cae55357f4c9c57d48d29be2c4cba493cb0c2
speg03 <speg03@gmail.com>
2013-07-16 23:53:05 +0900

    markdown-modeのM-n, M-pを無効化した。

--- .emacs.d/conf/50-markdown.el
+++ .emacs.d/conf/50-markdown.el
@@ -2,4 +2,6 @@
 
 (when (require 'markdown-mode nil t)
   (add-to-list 'auto-mode-alist '("\\.md" . markdown-mode))
+  (define-key markdown-mode-map (kbd "M-n") nil)
+  (define-key markdown-mode-map (kbd "M-p") nil)
   (setq markdown-command "pandoc"))
