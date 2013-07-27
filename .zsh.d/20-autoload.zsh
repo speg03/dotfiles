3c75cfb1b906d92f695d313bd50c148d34c04e6c
speg03 <speg03@gmail.com>
2013-07-27 11:19:55 +0900

    Add zsh completion directory.

--- .zsh.d/20-autoload.zsh
+++ .zsh.d/20-autoload.zsh
@@ -1,5 +1,7 @@
 # -*- coding: utf-8; mode: shell-script; -*-
 
+fpath=(~/.zsh.d/completion $fpath)
+
 autoload -Uz add-zsh-hook
 autoload -Uz colors
 autoload -Uz compinit
@@ -7,4 +9,4 @@ autoload -Uz is-at-least
 autoload -Uz vcs_info
 
 colors
-compinit
+compinit -u
