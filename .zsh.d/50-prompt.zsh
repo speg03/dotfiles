27bc16588c6e05ff792f927995c316b9bbe266a5
speg03 <speg03@gmail.com>
2013-07-27 10:16:12 +0900

    Minor change prompt.

--- .zsh.d/50-prompt.zsh
+++ .zsh.d/50-prompt.zsh
@@ -7,7 +7,7 @@ if is-at-least 4.3.10; then
     zstyle ':vcs_info:git:*' formats '%b' '%c' '%u' '%m'
     zstyle ':vcs_info:git:*' actionformats '%b' '%c' '%u' '%m' '%a'
     zstyle ':vcs_info:git:*' check-for-changes true
-    zstyle ':vcs_info:git:*' stagedstr 'M'    # %c で表示する文字列
+    zstyle ':vcs_info:git:*' stagedstr 'A'    # %c で表示する文字列
     zstyle ':vcs_info:git:*' unstagedstr 'M'  # %u で表示する文字列
 fi
 
@@ -29,10 +29,10 @@ function _vcs_info_prompt() {
         # vcs_info で何も取得していない場合はプロンプトを表示しない
         vcs_message=""
     else
-        vcs_b="%F{cyan}(${vcs_info_msg_0_})%f"
-        [ -n "${vcs_info_msg_1_}" ] && vcs_c=" %F{green}${vcs_info_msg_1_}%f"
-        [ -n "${vcs_info_msg_2_}" ] && vcs_u=" %F{red}${vcs_info_msg_2_}%f"
-        [ -n "${vcs_info_msg_3_}" ] && vcs_m=" %F{yellow}${vcs_info_msg_3_}%f"
+        vcs_b="%F{cyan}(${vcs_info_msg_0_})%f "
+        [ -n "${vcs_info_msg_1_}" ] && vcs_c="%F{green}${vcs_info_msg_1_}%f"
+        [ -n "${vcs_info_msg_2_}" ] && vcs_u="%F{red}${vcs_info_msg_2_}%f"
+        [ -n "${vcs_info_msg_3_}" ] && vcs_m="%F{yellow}${vcs_info_msg_3_}%f"
         [ -n "${vcs_info_msg_4_}" ] && vcs_a=" %F{red}<!${vcs_info_msg_4_}>%f"
 
         vcs_message="${vcs_b}${vcs_c}${vcs_u}${vcs_m}${vcs_a}"
