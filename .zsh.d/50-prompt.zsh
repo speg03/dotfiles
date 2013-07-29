3ed176d42247ed842acc5cb4545e1040ba0f8deb
speg03 <speg03@gmail.com>
2013-07-30 02:33:05 +0900

    Update vcs_info prompt. Add unmerged, unpushed, untracked marks.

--- .zsh.d/50-prompt.zsh
+++ .zsh.d/50-prompt.zsh
@@ -1,27 +1,72 @@
 # -*- coding: utf-8; mode: shell-script; -*-
 
-zstyle ':vcs_info:*' max-exports 5
+zstyle ':vcs_info:*' max-exports 4
 zstyle ':vcs_info:*' enable git
 
 if is-at-least 4.3.10; then
-    zstyle ':vcs_info:git:*' formats '%b' '%c' '%u' '%m'
-    zstyle ':vcs_info:git:*' actionformats '%b' '%c' '%u' '%m' '%a'
+    zstyle ':vcs_info:git:*' formats '%b' '%c' '%u'
+    zstyle ':vcs_info:git:*' actionformats '%b' '%c' '%u' '%a'
     zstyle ':vcs_info:git:*' check-for-changes true
     zstyle ':vcs_info:git:*' stagedstr 'A'    # %c で表示する文字列
     zstyle ':vcs_info:git:*' unstagedstr 'M'  # %u で表示する文字列
 fi
 
+### ベースのプロンプト
 function _base_prompt() {
     print -Pn '%F{green}%n@%m%f:%F{yellow}%1~%f'
 }
 
+### ブランチ名
+function _vcs_info_branch_prompt() {
+    if [ -n "$1" ]; then
+        print -Pn "%F{cyan}($1)%f"
+    fi
+}
+
+### staged
+function _vcs_info_staged_prompt() {
+    print -Pn '%F{green}'
+
+    ### git fetch して merge していないコミットがある場合
+    if command git status -b --porcelain | \grep '^##.*behind' >/dev/null 2>&1; then
+        print -Pn 'F'
+    fi
+
+    ### git commit して push していないコミットがある場合
+    if command git status -b --porcelain | \grep '^##.*ahead' >/dev/null 2>&1; then
+        print -Pn 'C'
+    fi
+
+    ### staged なファイルがある場合
+    if [ -n "$1" ]; then
+        print -Pn "$1"
+    fi
+    print -Pn '%f'
+}
+
+### unstaged なファイルがある場合
+function _vcs_info_unstaged_prompt() {
+    if [ -n "$1" ]; then
+        print -Pn "%F{red}$1%f"
+    fi
+}
+
+### untracked なファイルがある場合
+function _vcs_info_untracked_prompt() {
+    if command git status --porcelain | \grep '^??' >/dev/null 2>&1; then
+        print -Pn "%F{yellow}?%f"
+    fi
+}
+
+### conflict などが起こった場合
+function _vcs_info_action_prompt() {
+    if [ -n "$1" ]; then
+        print -Pn "%F{red}<!$1>%f"
+    fi
+}
+
 function _vcs_info_prompt() {
     local vcs_message
-    local vcs_b
-    local vcs_c
-    local vcs_u
-    local vcs_m
-    local vcs_a
 
     LANG=en_US.UTF-8 vcs_info
 
@@ -29,13 +74,12 @@ function _vcs_info_prompt() {
         # vcs_info で何も取得していない場合はプロンプトを表示しない
         vcs_message=""
     else
-        vcs_b="%F{cyan}(${vcs_info_msg_0_})%f "
-        [ -n "${vcs_info_msg_1_}" ] && vcs_c="%F{green}${vcs_info_msg_1_}%f"
-        [ -n "${vcs_info_msg_2_}" ] && vcs_u="%F{red}${vcs_info_msg_2_}%f"
-        [ -n "${vcs_info_msg_3_}" ] && vcs_m="%F{yellow}${vcs_info_msg_3_}%f"
-        [ -n "${vcs_info_msg_4_}" ] && vcs_a=" %F{red}<!${vcs_info_msg_4_}>%f"
-
-        vcs_message="${vcs_b}${vcs_c}${vcs_u}${vcs_m}${vcs_a}"
+        # 各状態のマークを連結する
+        vcs_message="`_vcs_info_branch_prompt ${vcs_info_msg_0_}` "
+        vcs_message+="`_vcs_info_staged_prompt ${vcs_info_msg_1_}`"
+        vcs_message+="`_vcs_info_unstaged_prompt ${vcs_info_msg_2_}`"
+        vcs_message+="`_vcs_info_untracked_prompt`"
+        vcs_message+=" `_vcs_info_action_prompt ${vcs_info_msg_3_}`"
     fi
 
     print -Pn ${vcs_message}
