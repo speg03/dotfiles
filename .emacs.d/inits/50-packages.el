35fd15aafe9e716409bb9e4aed992b7d2205e9e7
speg03 <speg03@gmail.com>
2014-03-06 13:52:29 +0900

    Merge elisp for package configurations.

--- .emacs.d/inits/50-packages.el
+++ .emacs.d/inits/50-packages.el
@@ -1,7 +1,15 @@
 ;;; -*- coding: utf-8; mode: emacs-lisp; -*-
 
+;; git-commit-mode
+(install-and-require 'git-commit-mode)
+
+;; markdown-mode
 (when (install-and-require 'markdown-mode)
   (add-to-list 'auto-mode-alist '("\\.md" . markdown-mode))
   (define-key markdown-mode-map (kbd "M-n") nil)
   (define-key markdown-mode-map (kbd "M-p") nil)
   (setq markdown-command "pandoc"))
+
+;; undo-tree
+(when (install-and-require 'undo-tree)
+  (global-undo-tree-mode))
--- .emacs.d/inits/50-undo-tree.el
+++
@@ -1,4 +0,0 @@
-;;; -*- coding: utf-8; mode: emacs-lisp; -*-
-
-(when (install-and-require 'undo-tree)
-  (global-undo-tree-mode t))
