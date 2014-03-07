0014c1783d1e918c6ecd7afbd11122f09daff039
speg03 <speg03@gmail.com>
2014-03-08 00:51:27 +0900

    cosmetic changes

--- .emacs.d/inits/50-packages.el
+++ .emacs.d/inits/50-packages.el
@@ -27,7 +27,7 @@
 
 ;; markdown-mode
 (when (install-and-require 'markdown-mode)
-  (add-to-list 'auto-mode-alist '("\\.md" . markdown-mode))
+  (add-to-list 'auto-mode-alist '("\\.md$" . markdown-mode))
   (define-key markdown-mode-map (kbd "M-n") nil)
   (define-key markdown-mode-map (kbd "M-p") nil)
   (setq markdown-command "pandoc"))
