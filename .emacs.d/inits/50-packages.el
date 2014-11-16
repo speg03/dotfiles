4eaf2aba35da622cd10d479ab29106013b56312f
Takahiro Yano <speg03@gmail.com>
2014-11-16 18:01:27 +0900

    set indent-offset for web-mode

--- .emacs.d/inits/50-packages.el
+++ .emacs.d/inits/50-packages.el
@@ -48,6 +48,9 @@
 
 ;; web-mode
 (when (install-and-require 'web-mode)
+  (setq web-mode-markup-indent-offset 2)
+  (setq web-mode-css-indent-offset 2)
+  (setq web-mode-code-indent-offset 2)
   (add-to-list 'auto-mode-alist '("\\.js\\'" . web-mode))
   (add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
   (add-to-list 'auto-mode-alist '("\\.html\\'" . web-mode)))
