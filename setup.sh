310ce0a1b5cdb0c41c5ea026d436518b4a25d2ae
Takahiro Yano <speg03@gmail.com>
2015-01-17 09:40:37 +0000

    Add docker zsh completion and rbenv

--- setup.sh
+++ setup.sh
@@ -20,9 +20,18 @@ do
     ln -snf $basedir/$rc $HOME
 done
 
-## enable git-completion for zsh
-if [ "`uname`" = "Linux" ]; then
-    cd $basedir/.zsh.d/completion
-    curl -L https://raw.github.com/git/git/master/contrib/completion/git-completion.zsh -o _git
-    curl -LO https://raw.github.com/git/git/master/contrib/completion/git-completion.bash
+## enable completion for zsh
+if [ "$(uname)" = "Linux" ]; then
+    curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.zsh \
+        -o $HOME/.zsh.d/completion/_git
+    curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash \
+        -o $HOME/.zsh.d/completion/git-completion.bash
+    curl https://raw.githubusercontent.com/docker/docker/master/contrib/completion/zsh/_docker \
+        -o $HOME/.zsh.d/completion/_docker
+fi
+
+## rbenv
+if [ ! -e $HOME/.rbenv ]; then
+    git clone https://github.com/sstephenson/rbenv $HOME/.rbenv
+    git clone https://github.com/sstephenson/ruby-build $HOME/.rbenv/plugins/ruby-build
 fi
