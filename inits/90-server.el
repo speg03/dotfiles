b40f88477b1d6c2602fded78c7e38bc3e1280776
Takahiro Yano <speg03@gmail.com>
2015-06-18 18:58:37 +0900

    Add server-start

--- inits/90-server.el
+++ inits/90-server.el
@@ -1,5 +1,9 @@
 (require 'server)
 
+(when (eq window-system 'w32)
+  (unless (server-running-p)
+    (server-start)))
+
 ;;; Do not confirm server-kill-buffer
 (when (server-running-p)
   (remove-hook 'kill-buffer-query-functions 'server-kill-buffer-query-function))
