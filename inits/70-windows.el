d0e458ccd94a30df76a2fde8b8d911a46ea482a7
Takahiro Yano <speg03@gmail.com>
2015-06-19 17:21:00 +0900

    Assign C-x C-c to suspend-frame instead of kill-emacs

--- inits/70-windows.el
+++ inits/70-windows.el
@@ -2,4 +2,12 @@
 
 ;; for Windows
 (when (eq window-system 'w32)
-  (set-file-name-coding-system 'cp932))
+  (set-file-name-coding-system 'cp932)
+
+  ;; Assign C-x C-c to suspend-frame instead of kill-emacs
+  (define-key global-map (kbd "C-x C-c")
+    '(lambda ()
+       (interactive)
+       (progn
+         (kill-this-buffer)
+         (suspend-frame)))))
