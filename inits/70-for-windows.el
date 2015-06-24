f4c0ca091b50f606294eaac76883c00aa685edb7
Takahiro Yano <speg03@gmail.com>
2015-06-24 13:19:26 +0900

    Add font configuration

--- inits/70-for-windows.el
+++ inits/70-for-windows.el
@@ -4,6 +4,14 @@
 (when (eq window-system 'w32)
   (set-file-name-coding-system 'cp932)
 
+  ;; Font
+  (set-face-attribute 'default nil
+                      :family "源ノ角ゴシック Code JP R"
+                      :height 110)
+  (set-fontset-font (frame-parameter nil 'font)
+                    'japanese-jisx0208
+                    (font-spec :family "源ノ角ゴシック Code JP M"))
+
   ;; Assign C-x C-c to suspend-frame instead of kill-emacs
   (define-key global-map (kbd "C-x C-c")
     '(lambda ()
