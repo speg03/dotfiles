9b56a98ad46dfd55420af701eb3466153dfe547d
Takahiro Yano <speg03@gmail.com>
2015-06-26 00:34:04 +0900

    Add a condition for Mac terminals

--- inits/70-for-mac.el
+++ inits/70-for-mac.el
@@ -1,17 +1,22 @@
 ;;; -*- coding: utf-8; mode: emacs-lisp; -*-
 
-;; for Mac GUI
-(when (eq window-system 'ns)
+;; for Mac
+(when (eq system-type 'darwin)
   ;; Remap Command to Meta
   (setq ns-command-modifier (quote meta))
 
-  ;; Font
-  (set-face-attribute 'default nil
-                      :family "源ノ角ゴシック Code JP R"
-                      :height 120)
-  (set-fontset-font (frame-parameter nil 'font)
-                    'japanese-jisx0208
-                    (font-spec :family "源ノ角ゴシック Code JP M"))
+  ;; for Mac GUI
+  (when (eq window-system 'ns)
+    ;; Font
+    (set-face-attribute 'default nil
+                        :family "源ノ角ゴシック Code JP R"
+                        :height 120)
+    (set-fontset-font (frame-parameter nil 'font)
+                      'japanese-jisx0208
+                      (font-spec :family "源ノ角ゴシック Code JP M"))
 
-  ;; End
+    ;; End of window-system
+    )
+
+  ;; End of system-type
   )
