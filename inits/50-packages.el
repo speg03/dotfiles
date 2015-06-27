9202e033f62feec6bdf460bc9305d17f9a74b102
Takahiro Yano <speg03@gmail.com>
2015-06-27 14:11:42 +0900

    Disable magit startup message

--- inits/50-packages.el
+++ inits/50-packages.el
@@ -30,6 +30,8 @@
 
 ;; magit
 (when (install-and-require 'magit)
+  (setq magit-auto-revert-mode nil)
+  (setq magit-last-seen-setup-instructions "1.4.0")
   (global-set-key (kbd "C-x g") 'magit-status))
 
 ;; markdown-mode
