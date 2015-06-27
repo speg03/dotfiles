6b587b7c92a31bffbdfe7c9efac15dcd2a962aa8
Takahiro Yano <speg03@gmail.com>
2015-06-27 12:54:57 +0900

    cosmetic changes

--- inits/70-for-mac.el
+++ inits/70-for-mac.el
@@ -1,12 +1,12 @@
 ;;; -*- coding: utf-8; mode: emacs-lisp; -*-
 
-;; for Mac
+;; Configurations for Mac
 (when (eq system-type 'darwin)
   ;; Remap Command to Meta
   (setq ns-command-modifier (quote meta))
 
-  ;; for Mac GUI
-  (when (eq window-system 'ns)
+  ;; for GUI
+  (when window-system
     ;; Font
     (set-face-attribute 'default nil
                         :family "源ノ角ゴシック Code JP R"
@@ -14,9 +14,5 @@
     (set-fontset-font (frame-parameter nil 'font)
                       'japanese-jisx0208
                       (font-spec :family "源ノ角ゴシック Code JP R"))
-
-    ;; End of window-system
-    )
-
-  ;; End of system-type
-  )
+    ) ; End for GUI
+  ) ; End for configurations
--- inits/70-for-windows.el
+++ inits/70-for-windows.el
@@ -1,24 +1,25 @@
 ;;; -*- coding: utf-8; mode: emacs-lisp; -*-
 
-;; for Windows GUI
-(when (eq window-system 'w32)
+;; Configurations for Windows
+(when (eq system-type 'windows-nt)
   (set-file-name-coding-system 'cp932)
 
-  ;; Font
-  (set-face-attribute 'default nil
-                      :family "源ノ角ゴシック Code JP R"
-                      :height 110)
-  (set-fontset-font (frame-parameter nil 'font)
-                    'japanese-jisx0208
-                    (font-spec :family "源ノ角ゴシック Code JP R"))
+  ;; for GUI
+  (when window-system
+    ;; Font
+    (set-face-attribute 'default nil
+                        :family "源ノ角ゴシック Code JP R"
+                        :height 110)
+    (set-fontset-font (frame-parameter nil 'font)
+                      'japanese-jisx0208
+                      (font-spec :family "源ノ角ゴシック Code JP R"))
 
-  ;; Assign C-x C-c to suspend-frame instead of kill-emacs
-  (define-key global-map (kbd "C-x C-c")
-    '(lambda ()
-       (interactive)
-       (progn
-         (kill-this-buffer)
-         (suspend-frame))))
-
-  ;; End
-  )
+    ;; Assign C-x C-c to suspend-frame instead of kill-emacs
+    (define-key global-map (kbd "C-x C-c")
+      '(lambda ()
+         (interactive)
+         (progn
+           (kill-this-buffer)
+           (suspend-frame))))
+    ) ; End for GUI
+  ) ; End for configurations
