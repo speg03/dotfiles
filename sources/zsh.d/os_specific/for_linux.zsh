a758a2b87894cc41f19fb8a2fe4a8df12b903d32
Takahiro Yano <speg03@gmail.com>
2016-02-17 16:52:35 +0900

    alias o to open/xdg-open

--- sources/zsh.d/os_specific/for_linux.zsh
+++ sources/zsh.d/os_specific/for_linux.zsh
@@ -1,5 +1,6 @@
 # -*- coding: utf-8; mode: shell-script; -*-
 
 alias ls='ls --color=auto'
+alias o='xdg-open'
 
 source $HOME/.zsh.d/os_specific/net_tools_deprecated
--- sources/zsh.d/os_specific/for_mac.zsh
+++ sources/zsh.d/os_specific/for_mac.zsh
@@ -1,3 +1,4 @@
 # -*- coding: utf-8; mode: shell-script; -*-
 
 alias ls='ls -G'
+alias o='open'
