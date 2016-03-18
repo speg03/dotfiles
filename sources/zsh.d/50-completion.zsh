a4af7e9bc6a44e3e43e4510362ae8bf0d5796069
Takahiro Yano <speg03@gmail.com>
2016-03-18 14:00:16 +0900

    Add completion for hosts

--- sources/zsh.d/50-completion.zsh
+++ sources/zsh.d/50-completion.zsh
@@ -1,5 +1,12 @@
 # -*- coding: utf-8; mode: shell-script; -*-
 
+function print_known_hosts (){
+    if [ -f $HOME/.ssh/known_hosts ]; then
+        cat $HOME/.ssh/known_hosts | tr ',' ' ' | cut -d' ' -f1
+    fi
+}
+_cache_hosts=($( print_known_hosts ))
+
 if [[ -f /usr/local/share/zsh/site-functions/_aws ]]; then
     source /usr/local/share/zsh/site-functions/_aws
 fi
