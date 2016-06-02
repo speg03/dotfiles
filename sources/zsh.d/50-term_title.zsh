0cb23e049a67feed16dde363f2144023ebce4eee
Takahiro Yano <speg03@gmail.com>
2016-06-02 11:02:28 +0900

    Fix the condition
    Suppress to change terminal window title.

--- sources/zsh.d/50-term_title.zsh
+++ sources/zsh.d/50-term_title.zsh
@@ -5,7 +5,7 @@ function _update_term_title() {
     print -Pn "\e]0;%~\a"
 }
 
-if [ "$TERM" = "screen" ]; then
+if [ -n "$TMUX" ]; then
     add-zsh-hook chpwd _update_term_title
     _update_term_title
 fi
