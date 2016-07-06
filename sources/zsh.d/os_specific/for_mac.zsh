4311414d0686b7ed305a17a7289ca6378cb237d6
Takahiro Yano <speg03@gmail.com>
2016-07-06 22:53:14 +0900

    Use original PATH for homebrew

--- sources/zsh.d/os_specific/for_mac.zsh
+++ sources/zsh.d/os_specific/for_mac.zsh
@@ -3,5 +3,7 @@
 alias ls='ls -G'
 alias o='open'
 
+alias brew='PATH=$BASEPATH brew'
+
 alias dclean='docker ps -q -f status=exited | xargs docker rm'
 alias dcleani='docker images -q -f dangling=true | xargs docker rmi'
