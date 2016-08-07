70c97fc1c78fc10007e00ec0f8fc3ec30b67dcf8
Takahiro Yano <speg03@gmail.com>
2016-08-07 14:11:45 +0900

    Add fontconfig path for octave with jupyter notebook

--- sources/zsh.d/os_specific/for_mac.zsh
+++ sources/zsh.d/os_specific/for_mac.zsh
@@ -7,3 +7,5 @@ alias brew='PATH=$BASEPATH brew'
 
 alias dclean='docker ps -q -f status=exited | xargs docker rm'
 alias dcleani='docker images -q -f dangling=true | xargs docker rmi'
+
+alias jnb='FONTCONFIG_PATH=/opt/X11/lib/X11/fontconfig jupyter-notebook'
