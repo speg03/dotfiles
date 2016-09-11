052836f46bf8ecd825cd44a5c6ae28c28763bb28
Takahiro Yano <speg03@gmail.com>
2016-09-11 21:23:09 +0900

    Prevent error messages by open for Mac in tmux

--- sources/zsh.d/os_specific/for_linux.zsh
+++ sources/zsh.d/os_specific/for_linux.zsh
@@ -6,4 +6,6 @@ alias o='xdg-open'
 alias dclean='docker ps -q -f status=exited | xargs --no-run-if-empty docker rm'
 alias dcleani='docker images -q -f dangling=true | xargs --no-run-if-empty docker rmi'
 
+alias reattach-to-user-namespace=':'
+
 source $HOME/.zsh.d/os_specific/net_tools_deprecated
