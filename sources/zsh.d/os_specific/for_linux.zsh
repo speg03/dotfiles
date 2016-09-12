8f39b355888870ace3f7ac8888ef040eccb32417
Takahiro Yano <speg03@gmail.com>
2016-09-12 10:28:04 +0900

    Fix reattach-to-user-namespace workaround for Linux

--- sources/zsh.d/os_specific/for_linux.zsh
+++ sources/zsh.d/os_specific/for_linux.zsh
@@ -6,6 +6,13 @@ alias o='xdg-open'
 alias dclean='docker ps -q -f status=exited | xargs --no-run-if-empty docker rm'
 alias dcleani='docker images -q -f dangling=true | xargs --no-run-if-empty docker rmi'
 
-alias reattach-to-user-namespace=':'
+if [ ! -e $HOME/bin/reattach-to-user-namespace ]; then
+    mkdir -p $HOME/bin
+    cat <<EOF >$HOME/bin/reattach-to-user-namespace
+#!/bin/sh
+exec $SHELL
+EOF
+    chmod +x $HOME/bin/reattach-to-user-namespace
+fi
 
 source $HOME/.zsh.d/os_specific/net_tools_deprecated
