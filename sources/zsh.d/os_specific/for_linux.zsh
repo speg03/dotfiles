9c47bc6f9580675eb6a3dce6e6759bb74f3efeea
Takahiro Yano <speg03@gmail.com>
2016-10-03 21:06:54 +0900

    Revise tmux default-command

--- sources/zsh.d/os_specific/for_linux.zsh
+++ sources/zsh.d/os_specific/for_linux.zsh
@@ -6,13 +6,6 @@ alias o='xdg-open'
 alias dclean='docker ps -q -f status=exited | xargs --no-run-if-empty docker rm'
 alias dcleani='docker images -q -f dangling=true | xargs --no-run-if-empty docker rmi'
 
-if [ ! -e $HOME/bin/reattach-to-user-namespace ]; then
-    mkdir -p $HOME/bin
-    cat <<'EOF' >$HOME/bin/reattach-to-user-namespace
-#!/bin/sh
-exec $SHELL
-EOF
-    chmod +x $HOME/bin/reattach-to-user-namespace
-fi
+export TMUX_DEFAULT_COMMAND=""
 
 source $HOME/.zsh.d/os_specific/net_tools_deprecated
--- sources/zsh.d/os_specific/for_mac.zsh
+++ sources/zsh.d/os_specific/for_mac.zsh
@@ -7,3 +7,5 @@ alias brew='PATH=$BASEPATH brew'
 
 alias dclean='docker ps -q -f status=exited | xargs docker rm'
 alias dcleani='docker images -q -f dangling=true | xargs docker rmi'
+
+export TMUX_DEFAULT_COMMAND="reattach-to-user-namespace -l $SHELL"
