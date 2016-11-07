5993250502a49e25b13ab20dcb0f68f3a9b7ffcc
Takahiro Yano <speg03@gmail.com>
2016-11-08 00:55:51 +0900

    Add export command to expose history variables

--- sources/zsh.d/30-general.zsh
+++ sources/zsh.d/30-general.zsh
@@ -3,6 +3,6 @@
 zstyle ':completion:*:default' menu select=1
 zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
 
-HISTFILE="$HOME/.zhistory"
-HISTSIZE=10000
-SAVEHIST=10000
+export HISTFILE="$HOME/.zhistory"
+export HISTSIZE=10000
+export SAVEHIST=10000
