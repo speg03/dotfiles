7a4479c9fde496f1b671bf58439db9c2a42b3d50
Takahiro Yano <speg03@gmail.com>
2016-11-19 01:12:08 +0900

    Remove elisp directory

--- init.el
+++ init.el
@@ -1,17 +1,5 @@
 ;;; -*- coding: utf-8; mode: emacs-lisp; -*-
 
-(defun add-to-load-path (&rest paths)
-  "Add paths to load-path recursively."
-  (let (path)
-    (dolist (path paths paths)
-      (let ((default-directory
-              (expand-file-name (concat user-emacs-directory path))))
-        (add-to-list 'load-path default-directory)
-        (if (fboundp 'normal-top-level-add-subdirs-to-load-path)
-            (normal-top-level-add-subdirs-to-load-path))))))
-
-(add-to-load-path "elisp")
-
 (setq emacs-version-short (replace-regexp-in-string
                            "\\([0-9]+\\)\\.\\([0-9]+\\).*"
                            "\\1_\\2" emacs-version))
