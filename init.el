9d975c28ecc6f7342a166c5a61a77710389e80aa
Takahiro Yano <speg03@gmail.com>
2016-11-19 01:33:52 +0900

    Use locate-user-emacs-file

--- init.el
+++ init.el
@@ -1,11 +1,11 @@
 ;;; -*- coding: utf-8; mode: emacs-lisp; -*-
 
-(setq emacs-version-short (replace-regexp-in-string
-                           "\\([0-9]+\\)\\.\\([0-9]+\\).*"
-                           "\\1_\\2" emacs-version))
-(setq custom-file (expand-file-name
-                   (concat "custom/custom_" emacs-version-short ".el")
-                   user-emacs-directory))
+(setq emacs-version-short
+      (replace-regexp-in-string
+       "\\([0-9]+\\)\\.\\([0-9]+\\).*" "\\1_\\2" emacs-version))
+(setq custom-file
+      (locate-user-emacs-file
+       (concat "custom/custom_" emacs-version-short ".el")))
 
 (defun install-package-if-not-exist (pkg)
   "Install the package if it does not exist."
@@ -15,11 +15,12 @@
   (when (package-installed-p pkg)
     (message "INFO: Loaded %s." pkg)))
 
-(when (file-exists-p "~/.emacs.d/proxy.el")
-  (load-file "~/.emacs.d/proxy.el"))
+(when (file-exists-p (locate-user-emacs-file "proxy.el"))
+  (load-file (locate-user-emacs-file "proxy.el")))
 
 (when (require 'package nil t)
-  (setq package-user-dir "~/.emacs.d/packages/")
+  (setq package-user-dir
+        (locate-user-emacs-file "packages/"))
   (add-to-list 'package-archives
                '("melpa" . "http://melpa.milkbox.net/packages/") t)
   (package-initialize)
@@ -27,4 +28,4 @@
 
   (when (install-package-if-not-exist 'init-loader)
     (require 'init-loader)
-    (init-loader-load "~/.emacs.d/inits/")))
+    (init-loader-load (locate-user-emacs-file "inits/"))))
