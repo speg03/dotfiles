7bff3efe46628da952c450bc6224e253744433b4
Takahiro Yano <speg03@gmail.com>
2016-11-30 10:30:09 +0900

    Add mozc for Linux

--- inits/os_specific/for_linux.el
+++ inits/os_specific/for_linux.el
@@ -12,6 +12,13 @@
   (setenv "PYENV_ROOT"
           (concat (getenv "HOME") "/.anyenv/envs/pyenv"))
 
+  ;; Mozc
+  (when (install-package-if-not-exist 'mozc)
+    (require 'mozc)
+    (setq default-input-method "japanese-mozc")
+    (global-set-key (kbd "<henkan>") 'toggle-input-method)
+    (define-key mozc-mode-map (kbd "<muhenkan>") 'toggle-input-method))
+
   ;; Font size
   (setq default-face-attribute-height 110)
   (set-face-attribute 'default nil
