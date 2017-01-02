a7eefa1ce709014c7ea9d6d08e720c188ab10ee6
Takahiro Yano <speg03@gmail.com>
2017-01-02 23:26:44 +0900

    Remove net_tools_deprecated

--- sources/zsh.d/os_specific/for_linux.zsh
+++ sources/zsh.d/os_specific/for_linux.zsh
@@ -5,5 +5,3 @@ alias dclean='docker ps -q -f status=exited | xargs --no-run-if-empty docker rm'
 alias dcleani='docker images -q -f dangling=true | xargs --no-run-if-empty docker rmi'
 
 export TMUX_DEFAULT_COMMAND=""
-
-source $HOME/.zsh.d/os_specific/net_tools_deprecated
