6bb0e801d6f77dde6ac9d552390a78b795ae482b
Takahiro Yano <speg03@gmail.com>
2017-01-06 23:37:30 +0900

    Toggle tmux keybind

--- sources/zsh.d/60-bindkey.zsh
+++ sources/zsh.d/60-bindkey.zsh
@@ -1,4 +1,4 @@
 bindkey -e
 bindkey '^P' history-beginning-search-backward
 bindkey '^N' history-beginning-search-forward
-bindkey -s '^jd' "tmux attach\n"
+bindkey -s '^jd' "tmux new-session -A -s main\n"
