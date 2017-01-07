dca32041e274101b5c3d627dfb95a7fa998081d6
Takahiro Yano <speg03@gmail.com>
2017-01-07 23:54:59 +0900

    Apply local to variables in function

--- sources/zsh.d/50-functions.zsh
+++ sources/zsh.d/50-functions.zsh
@@ -2,7 +2,7 @@
 
 function color_map() {
     for color in $(seq 0 255); do
-        formatted_number=$(printf '%3d' $color)
+        local formatted_number=$(printf '%3d' $color)
         print -Pn " %F{$color}$formatted_number%f"
         if [ $(expr $color % 8) -eq 7 ]; then
             echo
@@ -12,9 +12,10 @@ function color_map() {
 
 function hostname_color() {
     # Convert hostname to number (0 - 255)
-    hostname_hash_number=$(printf '%d' 0x$(hostname | md5sum | cut -c1-2))
+    local hostname_hash=$(hostname | md5sum | cut -c1-2)
+    local hostname_hash_number=$(printf '%d' 0x$hostname_hash)
     # Valid color range: 25 - 231
-    valid_color=$(expr $hostname_hash_number % \( 232 - 25 \) + 25)
+    local valid_color=$(expr $hostname_hash_number % \( 232 - 25 \) + 25)
     echo -n $valid_color
 }
 
@@ -22,7 +23,7 @@ function hostname_color() {
 ### Emacs
 
 function e() {
-    frame_count=$(emacsclient -a '' -e '(length (visible-frame-list))')
+    local frame_count=$(emacsclient -a '' -e '(length (visible-frame-list))')
     if [ $frame_count -lt 2 ]; then
         emacsclient -nc "$@"
     else
@@ -38,7 +39,7 @@ function kill-emacs() {
 ### Repository
 
 function g() {
-    repository=$(ghq list -p | peco)
+    local repository=$(ghq list -p | peco)
     if [ -n "$repository" ]; then
         cd $repository
     fi
