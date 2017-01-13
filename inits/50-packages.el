a813a7ee2b77c484b2732c26d1da4bd34f33fc4d
Takahiro Yano <speg03@gmail.com>
2017-01-14 07:43:52 +0900

    :shower: Refresh helm configurations
    HelmのGetting startedを参考に整理
    https://emacs-helm.github.io/helm/#getting-started

--- inits/50-packages.el
+++ inits/50-packages.el
@@ -43,11 +43,11 @@
 
 ;; helm
 (when (install-package-if-not-exist 'helm)
-  (require 'helm)
+  (require 'helm-config)
+  (helm-mode 1)
   (global-set-key (kbd "M-x") 'helm-M-x)
   (global-set-key (kbd "C-x C-f") 'helm-find-files)
-  (define-key helm-map (kbd "C-h") 'delete-backward-char)
-  (helm-mode 1))
+  (define-key helm-map (kbd "C-h") 'delete-backward-char))
 
 ;; magit
 (when (install-package-if-not-exist 'magit)
