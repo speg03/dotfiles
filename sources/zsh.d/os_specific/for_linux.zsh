19fb7c3e22fe0ad3886cfc4370921b2f53223c52
Takahiro Yano <speg03@gmail.com>
2017-01-26 08:37:00 +0900

    Revert ":shower: Remove tmux default-command"
    This reverts commit 166f68151fdf64df4713774b21c33e2f153f43bb.
    
    GUIのEmacsを起動したときにキーボード入力を受け付けなくなってしまったの
    で元に戻す

--- sources/zsh.d/os_specific/for_linux.zsh
+++ sources/zsh.d/os_specific/for_linux.zsh
@@ -3,3 +3,5 @@ alias o='xdg-open'
 
 alias dclean='docker ps -q -f status=exited | xargs --no-run-if-empty docker rm'
 alias dcleani='docker images -q -f dangling=true | xargs --no-run-if-empty docker rmi'
+
+export TMUX_DEFAULT_COMMAND=""
