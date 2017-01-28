c258a121758ccad474966e8360a120c83be9bef9
Takahiro Yano <speg03@gmail.com>
2017-01-28 23:09:04 +0900

    :+1: Start tmux with exec
    ログインシェル起動時の最初にtmuxを起動するように変更

--- sources/zsh.d/60-bindkey.zsh
+++ sources/zsh.d/60-bindkey.zsh
@@ -1,7 +1,6 @@
 bindkey -e
 bindkey '^P' history-beginning-search-backward
 bindkey '^N' history-beginning-search-forward
-bindkey -s '^jd' "$TMUX_NEW_COMMAND\n"
 
 ### fzf
 if [ -f $HOME/.fzf/shell/key-bindings.zsh ]; then
