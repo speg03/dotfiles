6549eee4c237a495b3d3a76ac8c295bf83982373
Takahiro Yano <speg03@gmail.com>
2017-01-29 17:35:05 +0900

    :recycle: Remove TMUX_DEFAULT_COMMAND variable
    環境変数を使わずにtmux.conf内でmacOSかどうかの分岐を追加
    
    See: https://github.com/ChrisJohnsen/tmux-MacOSX-pasteboard/blob/master/Usage.md#cross-platform-conditional-usage

--- sources/zsh.d/os_specific/for_mac.zsh
+++ sources/zsh.d/os_specific/for_mac.zsh
@@ -7,5 +7,3 @@ alias dclean='docker ps -q -f status=exited | xargs docker rm'
 alias dcleani='docker images -q -f dangling=true | xargs docker rmi'
 
 alias md5sum='md5'
-
-export TMUX_DEFAULT_COMMAND="exec reattach-to-user-namespace -l $SHELL"
