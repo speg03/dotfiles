287fbe5f2ec53edb1bf03fd1fb07630c2c4f8d57
Takahiro Yano <speg03@gmail.com>
2017-01-29 23:32:50 +0900

    :recycle: Move some variables from zshrc to zprofile
    zshrcの中で設定していた環境変数をzprofileへ移動した。
    ヒストリー関連やlessのオプションは対話環境のものなのでzprofileで設定す
    る。

--- sources/zsh.d/rcs/30-general.zsh
+++ sources/zsh.d/rcs/30-general.zsh
@@ -1,8 +1,2 @@
 zstyle ':completion:*:default' menu select=1
 zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
-
-export HISTFILE="$HOME/.zhistory"
-export HISTSIZE=10000
-export SAVEHIST=10000
-
-export LESS='-giMR'
