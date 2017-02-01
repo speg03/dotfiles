06d7839d3c51b51508d9d113bce1e63a748f46dc
Takahiro Yano <speg03@gmail.com>
2017-02-01 22:18:20 +0900

    :sparkles: Add git-status for vcs prompt
    git statusのahead/behindをプロンプトに表示

--- sources/zsh.d/rcs/40-prompt.zsh
+++ sources/zsh.d/rcs/40-prompt.zsh
@@ -8,6 +8,24 @@ zstyle ':vcs_info:git:*' actionformats \
        '%F{cyan}(%b)%f %F{green}%c%f%F{red}%u%f%F{magenta}%m%f%F{red}<!%a>%f'
 zstyle ':vcs_info:git:*' check-for-changes true
 
+zstyle ':vcs_info:git*+set-message:*' hooks git-st
+function +vi-git-st() {
+    local ahead behind
+    local -a gitstatus
+
+    ahead=$(git rev-list ${hook_com[branch]}@{upstream}..HEAD 2>/dev/null \
+                | wc -l | awk '{print $1}')
+    (( $ahead )) && gitstatus+=( "+${ahead}" )
+
+    behind=$(git rev-list HEAD..${hook_com[branch]}@{upstream} 2>/dev/null \
+                 | wc -l | awk '{print $1}')
+    (( $behind )) && gitstatus+=( "-${behind}" )
+
+    if [[ ${#gitstatus[@]} > 0 ]]; then
+        hook_com[misc]="[${(j:/:)gitstatus}]"
+    fi
+}
+
 function _update_prompt() {
     local base_prompt
     local vcs_prompt
