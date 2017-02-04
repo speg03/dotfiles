c97290f0b7d346f3248fd0932023d8e797c5b83e
Takahiro Yano <speg03@gmail.com>
2017-02-04 21:50:41 +0900

    :shower: Update setopts
    不要なオプションの整理と記法の統一

--- sources/zsh.d/rcs/30-setopt.zsh
+++ sources/zsh.d/rcs/30-setopt.zsh
@@ -1,36 +1,41 @@
-unsetopt PROMPT_CR
-setopt PROMPT_SUBST
+# -*- coding: utf-8; -*-
 
-setopt APPEND_HISTORY
-setopt AUTO_LIST
-setopt AUTO_MENU
-setopt AUTO_PARAM_KEYS
-setopt AUTO_PARAM_SLASH
-setopt AUTO_RESUME
-setopt NO_BEEP
-setopt BRACE_CCL
-setopt CORRECT
-setopt EQUALS
-setopt EXTENDED_GLOB
-setopt NO_FLOW_CONTROL
-setopt HIST_IGNORE_DUPS
-setopt HIST_IGNORE_SPACE
-setopt HIST_VERIFY
-setopt NO_HUP
-setopt IGNORE_EOF
-setopt INTERACTIVE_COMMENTS
-setopt LIST_TYPES
-setopt LONG_LIST_JOBS
-setopt MAGIC_EQUAL_SUBST
+## Expansion and Globbing
+# グロブを展開するときにディレクトリに/を付ける
 setopt MARK_DIRS
-setopt MULTIOS
-setopt NUMERIC_GLOB_SORT
-setopt PRINT_EIGHTBIT
-setopt PRINT_EXIT_VALUE
-setopt PUSHD_IGNORE_DUPS
-setopt SHORT_LOOPS
-setopt SHARE_HISTORY
-setopt HIST_NO_STORE
-setopt PUSHD_SILENT
 
+## History
+# ヒストリファイルへ追記する
+setopt APPEND_HISTORY
+# ヒストリリスト内で重複させない
+setopt HIST_IGNORE_ALL_DUPS
+
+## Input/Output
+# コマンドのスペルミスを修正する
+setopt CORRECT
+# C-sによるフロー制御を無効にする
+setopt NO_FLOW_CONTROL
+# C-d(EOF)でシェルを終了しない
+setopt IGNORE_EOF
+# 対話シェルでも#以降をコメントとみなす
+setopt INTERACTIVE_COMMENTS
+# コマンドが0以外で終了したときに値を表示する
+setopt PRINT_EXIT_VALUE
+# 左辺=右辺の形式になっている引数でファイル名展開を行う
+setopt MAGIC_EQUAL_SUBST
+# 数字の付いたファイル名を文字順ではなく数値順に並べる
+setopt NUMERIC_GLOB_SORT
+
+## Job Control
+# シェルを終了するときに実行中ジョブにHUPを送らない
+setopt NO_HUP
+
+## Prompting
+# PROMPT変数の中で変数展開する
+setopt PROMPT_SUBST
+# コマンド実行時に右プロンプトを消去する
 setopt TRANSIENT_RPROMPT
+
+## Zle
+# ビープを抑止
+setopt NO_BEEP
