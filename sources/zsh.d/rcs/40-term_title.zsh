dd9f08743476f1a91b636500a4f1c2dbc112b389
Takahiro Yano <speg03@gmail.com>
2017-02-06 22:57:37 +0900

    :sparkles: Shorten long current path in tmux status line
    tmuxのステータスラインに表示するパスが長すぎる場合に短縮する

--- sources/zsh.d/rcs/40-term_title.zsh
+++ sources/zsh.d/rcs/40-term_title.zsh
@@ -1,8 +1,18 @@
 # -*- coding: utf-8; -*-
 
 function _update_term_title() {
+    local current_path=$(print -Pn '%~')
+    local length=${#current_path}
+
+    # 現在のパスを後ろから58文字以内に収める
+    if [[ $length > 58 ]]; then
+        # ドット3つ分を引いて55文字に縮める
+        local start=$(( length - 55 + 1 ))
+        current_path=...${current_path[$start,$length]}
+    fi
+
     # xtermのタイトルに現在のディレクトリパスを設定
-    print -Pn "\e]0;%~\a"
+    print -Pn "\e]0;$current_path\a"
 }
 
 if [ -n "$TMUX" ]; then
