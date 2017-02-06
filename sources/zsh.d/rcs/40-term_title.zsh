bd0eee9c1aa3ca405f0399e79ecaa8e7f2cb43b2
Takahiro Yano <speg03@gmail.com>
2017-02-06 23:11:16 +0900

    :art: Refactor
    使用している変数を減らして整理

--- sources/zsh.d/rcs/40-term_title.zsh
+++ sources/zsh.d/rcs/40-term_title.zsh
@@ -2,13 +2,12 @@
 
 function _update_term_title() {
     local current_path=$(print -Pn '%~')
-    local length=${#current_path}
 
     # 現在のパスを後ろから58文字以内に収める
-    if [[ $length > 58 ]]; then
+    if [[ ${#current_path} > 58 ]]; then
         # ドット3つ分を引いて55文字に縮める
-        local start=$(( length - 55 + 1 ))
-        current_path=...${current_path[$start,$length]}
+        local start=$(( ${#current_path} - 55 + 1 ))
+        current_path=...${current_path[$start,-1]}
     fi
 
     # xtermのタイトルに現在のディレクトリパスを設定
