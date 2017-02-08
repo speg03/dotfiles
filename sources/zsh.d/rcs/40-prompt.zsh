0fbeba4a795eaab5e87e117344c7c720f719f749
Takahiro Yano <speg03@gmail.com>
2017-02-08 21:05:28 +0900

    :bug: Fix condition for sizing current path
    文字数の比較が文字列比較になっていたため、数値比較に修正

--- sources/zsh.d/rcs/40-prompt.zsh
+++ sources/zsh.d/rcs/40-prompt.zsh
@@ -55,7 +55,7 @@ function _update_term_title() {
     local current_path=$(print -Pn '%~')
 
     # 現在のパスを後ろから58文字以内に収める
-    if [[ ${#current_path} > 58 ]]; then
+    if [[ ${#current_path} -gt 58 ]]; then
         # ドット3つ分を引いて55文字に縮める
         local start=$(( ${#current_path} - 55 + 1 ))
         current_path=...${current_path[$start,-1]}
