500b09aef3f293782e36c6f8203cbd7f4ceec769
Takahiro Yano <speg03@gmail.com>
2017-02-25 01:13:57 +0900

    :art: Apply Cica font for Mac
    Cicaフォントを設定

--- inits/os_specific/for_mac.el
+++ inits/os_specific/for_mac.el
@@ -6,6 +6,9 @@
 (setq ns-command-modifier (quote meta))
 
 ;; Font size
-(setq default-face-attribute-height 150)
+(setq default-face-attribute-height 190)
 (set-face-attribute 'default nil
                     :height default-face-attribute-height)
+
+;; Font
+(add-to-list 'default-frame-alist '(font . "CicaE"))
