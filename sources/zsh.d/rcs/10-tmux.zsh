698231990a1a82d476c045360f1f411a9f137f86
Takahiro Yano <speg03@gmail.com>
2017-03-16 22:55:36 +0900

    :sparkles: Add tmux sync symbol to status bar
    tmuxのpane synchronized状態がわかるようにステータスバーにアイコンを表
    示する。
    
    Unicode絵文字をtmux.confで使用できないようなので、zshrcからコマンドで
    設定するようにした。

---
+++ sources/zsh.d/rcs/10-tmux.zsh
@@ -0,0 +1,11 @@
+function _set_tmux_status() {
+    local sync_symbol=${TMUX_SYNC_SYMBOL:-$'\U1F503'}
+    local sync_enable="#[fg=brightred]"${sync_symbol}"#[default] "
+    local sync_disable="#[fg=brightblack]"${sync_symbol}"#[default] "
+    tmux set -g status-left "#{?pane_synchronized,$sync_enable,$sync_disable}"
+    tmux set -ag status-left "#[fg=cyan](#S)#[default] "
+}
+
+if [[ $TMUX ]]; then
+    _set_tmux_status
+fi
