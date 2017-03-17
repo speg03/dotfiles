dd080a2cafe483ea99527ecc5e70e896da05a50d
Takahiro Yano <speg03@gmail.com>
2017-03-17 09:08:31 +0900

    :art: You can change spaces following by tmux sync symbol
    tmux syncステータスの空白文字調整を環境変数で変更できるように修正

--- sources/zsh.d/rcs/10-tmux.zsh
+++ sources/zsh.d/rcs/10-tmux.zsh
@@ -1,7 +1,7 @@
 function _set_tmux_status() {
-    local sync_symbol=${TMUX_SYNC_SYMBOL:-$'\U1F503'}
-    local sync_enable="#[fg=brightred]"${sync_symbol}"#[default] "
-    local sync_disable="#[fg=brightblack]"${sync_symbol}"#[default] "
+    local sync_symbol=${TMUX_SYNC_SYMBOL:-$'\U1F503 '}
+    local sync_enable="#[fg=brightred]"${sync_symbol}"#[default]"
+    local sync_disable="#[fg=brightblack]"${sync_symbol}"#[default]"
     tmux set -g status-left "#{?pane_synchronized,$sync_enable,$sync_disable}"
     tmux set -ag status-left "#[fg=cyan](#S)#[default] "
 }
