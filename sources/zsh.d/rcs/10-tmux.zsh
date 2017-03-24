35a8c78fbd1c0f82bd7aded7328fbff2e2d7908a
Takahiro Yano <speg03@gmail.com>
2017-03-25 08:36:21 +0900

    :art: Update tmux status-left using powerline

--- sources/zsh.d/rcs/10-tmux.zsh
+++ sources/zsh.d/rcs/10-tmux.zsh
@@ -1,9 +1,16 @@
 function _set_tmux_status() {
     local sync_symbol=${TMUX_SYNC_SYMBOL:-$'\U1F503 '}
-    local sync_enable="#[fg=brightred]"${sync_symbol}"#[default]"
-    local sync_disable="#[fg=brightblack]"${sync_symbol}"#[default]"
-    tmux set -g status-left "#{?pane_synchronized,$sync_enable,$sync_disable}"
-    tmux set -ag status-left "#[fg=brightblue](#S)#[default] "
+    local sync_status
+    sync_status="#{?pane_synchronized,#[fg=brightred],#[fg=brightblack]}"
+    sync_status="${sync_status}${sync_symbol}#[fg=default]"
+
+    local status_line
+    status_line="#[bg=black]$sync_status"
+    status_line="${status_line}#[fg=black,bg=blue]"$'\UE0B0'
+    status_line="${status_line} #[fg=default,bold]#S#[nobold]"
+    status_line="${status_line} #[fg=blue,bg=default]"$'\UE0B0'"#[default] "
+
+    tmux set -g status-left "${status_line}"
 }
 
 if [[ $TMUX ]]; then
