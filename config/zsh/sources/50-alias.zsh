7fdae24c9b1a1fee85172fba52658f3f46715123
Takahiro Yano <speg03@gmail.com>
2017-06-02 08:53:34 +0900

    :sparkles: Add proxy commands

--- config/zsh/sources/50-alias.zsh
+++ config/zsh/sources/50-alias.zsh
@@ -18,6 +18,32 @@ reload() {
     exec "$SHELL" -l
 }
 
+### Network
+
+set-proxy() {
+    local proxy_conf
+    echo -n 'Proxy (host:port): '
+    read proxy_conf
+
+    export http_proxy="http://${proxy_conf}"
+    export https_proxy="${http_proxy}"
+    export no_proxy='127.0.0.1,localhost'
+}
+
+unset-proxy() {
+    unset http_proxy
+    unset https_proxy
+    unset no_proxy
+}
+
+save-proxy() {
+    {
+        echo "export http_proxy=${http_proxy}"
+        echo "export https_proxy=${https_proxy}"
+        echo "export no_proxy=${no_proxy}"
+    } >>$HOME/.config/zsh/zprofile.local
+}
+
 ### Docker
 
 alias d='docker'
