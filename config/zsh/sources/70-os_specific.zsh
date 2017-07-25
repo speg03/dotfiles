ad73f4775d51989268f0320a5fefe1d3d7e072aa
Takahiro Yano <speg03@gmail.com>
2017-07-25 23:16:05 +0900

    :fire: Remove zsh functions
    debug関数を環境変数によって挙動を変えるように変更して、
    さらにzshenvに移した。
    
    その結果不要になったfunctionsファイルを削除して、
    sourcesディレクトリの階層もなくした。

--- config/zsh/sources/70-os_specific.zsh
+++
@@ -1,6 +0,0 @@
-## Load OS specific configuration
-# OS: Linux, Darwin(macOS)
-if [[ -e $HOME/.config/zsh/sources/os_specific/$(uname -s).zsh ]]; then
-    debug "$HOME/.config/zsh/sources/os_specific/$(uname -s).zsh"
-    source $HOME/.config/zsh/sources/os_specific/$(uname -s).zsh
-fi
