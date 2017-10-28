1c855a123eec6cec6df64e15d72fb46a1c7de1e8
Takahiro Yano <speg03@gmail.com>
2017-10-29 00:20:58 +0900

    :art: Change sync status color in tmux status bar

--- config/zsh/70_tmux.zsh
+++ config/zsh/70_tmux.zsh
@@ -1,12 +1,12 @@
 _set_tmux_status() {
     local sync_symbol=${TMUX_SYNC_SYMBOL:-$'\U1F503 '}
     local sync_status
-    sync_status="#{?pane_synchronized,#[fg=brightred],#[fg=brightblack]}"
+    sync_status="#{?pane_synchronized,#[fg=brightred],#[fg=white]}"
     sync_status="${sync_status}${sync_symbol}#[fg=default]"
 
     local status_line
-    status_line="#[bg=black]${sync_status}"
-    status_line="${status_line}#[fg=black,bg=blue]"$'\UE0B0'
+    status_line="#[bg=brightblack]${sync_status}"
+    status_line="${status_line}#[fg=brightblack,bg=blue]"$'\UE0B0'
     status_line="${status_line} #[fg=default,bold]#S#[nobold]"
     status_line="${status_line} #[fg=blue,bg=default]"$'\UE0B0'"#[default] "
 
