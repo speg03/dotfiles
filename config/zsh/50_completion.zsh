0fbcd45298fba82f72b63cbcaff4e15819aca12d
Takahiro Yano <speg03@gmail.com>
2017-10-29 20:28:40 +0900

    :art: Rename source_if_exists to source-if-exists

--- config/zsh/50_completion.zsh
+++ config/zsh/50_completion.zsh
@@ -10,6 +10,6 @@ fpath=($DATA_HOME/zsh/completions(N-/) $fpath)
 compinit -i -u -d "$CACHE_HOME/zsh/zcompdump"
 
 
-source_if_exists "/usr/local/share/zsh/site-functions/_aws"
-source_if_exists "$SRC_HOME/github.com/junegunn/fzf/shell/completion.zsh"
-source_if_exists "$GOPATH/src/github.com/mattn/memo/misc/completion.zsh"
+source-if-exists "/usr/local/share/zsh/site-functions/_aws"
+source-if-exists "$SRC_HOME/github.com/junegunn/fzf/shell/completion.zsh"
+source-if-exists "$GOPATH/src/github.com/mattn/memo/misc/completion.zsh"
--- config/zsh/60_bindkey.zsh
+++ config/zsh/60_bindkey.zsh
@@ -4,4 +4,4 @@ bindkey '^N' history-beginning-search-forward
 
 bindkey '^]' change-repository
 
-source_if_exists "$SRC_HOME/github.com/junegunn/fzf/shell/key-bindings.zsh"
+source-if-exists "$SRC_HOME/github.com/junegunn/fzf/shell/key-bindings.zsh"
