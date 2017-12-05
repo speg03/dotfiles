f6245db51ba47553c7228f0a9ef378bf9af6b80f
Takahiro Yano <speg03@gmail.com>
2017-12-05 21:40:13 +0900

    :sparkles: Disable beep effects
    ビープ音を抑制する

--- config/emacs/90_misc.el
+++ config/emacs/90_misc.el
@@ -1,6 +1,9 @@
 ;; Disable startup-message
 (setq inhibit-startup-message t)
 
+;; Disable beep effects
+(setq ring-bell-function 'ignore)
+
 ;; Avoid "Symbolic link to SVN-controlled source file; follow link? (yes or no)"
 (setq vc-follow-symlinks t)
 
