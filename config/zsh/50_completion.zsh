cd6250e3f9bee7d16b6d2d8441a0e6410f375274
Takahiro Yano <speg03@gmail.com>
2018-07-22 15:23:11 +0900

    :fire: Remove unused completions

--- config/zsh/50_completion.zsh
+++ config/zsh/50_completion.zsh
@@ -9,7 +9,4 @@ fpath=($DATA_HOME/zsh/completions(N-/) $fpath)
 
 compinit -i -u -d "$CACHE_HOME/zsh/zcompdump"
 
-
-source-if-exists "/usr/local/share/zsh/site-functions/_aws"
 source-if-exists "$SRC_HOME/github.com/junegunn/fzf/shell/completion.zsh"
-source-if-exists "$GOPATH/src/github.com/mattn/memo/misc/completion.zsh"
