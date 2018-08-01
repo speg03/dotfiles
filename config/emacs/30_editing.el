23223c5826bb8ec65b4be64cca9a27a694872557
Takahiro Yano <speg03@gmail.com>
2018-08-01 23:52:23 +0900

    :children_crossing: Update bind for mc/edit-lines

--- config/emacs/30_editing.el
+++ config/emacs/30_editing.el
@@ -19,17 +19,26 @@
 
 (use-package multiple-cursors
   :after expand-region
-  :bind (("C-M-SPC" . mc/mark-all-dwim-or-mark-sexp)
+  :bind (("C-x r t" . mc/edit-lines-or-string-rectangle)
+         ("C-M-SPC" . mc/mark-all-dwim-or-mark-sexp)
          ("C-," . mc/mark-previous-like-this)
          ("C-." . mc/mark-next-like-this)
          ("C-<" . mc/skip-to-previous-like-this)
          ("C->" . mc/skip-to-next-like-this))
   :config
+  ;; ref: http://emacs.rubikitch.com/multiple-cursors/
+  (defun mc/edit-lines-or-string-rectangle (s e)
+    "C-x r tで同じ桁の場合にmc/edit-lines"
+    (interactive "r")
+    (if (eq (save-excursion (goto-char s) (current-column))
+            (save-excursion (goto-char e) (current-column)))
+        (call-interactively 'mc/edit-lines)
+      (call-interactively 'string-rectangle)))
+
   (defun mc/mark-all-dwim-or-mark-sexp (arg)
-    "C-u C-M-SPCでmc/mark-all-dwim, C-u C-u C-M-SPCでmc/edit-lines"
+    "C-u C-M-SPCでmc/mark-all-dwim"
     (interactive "p")
     (cl-case arg
-      (16 (mc/mark-all-dwim t))
       (4 (mc/mark-all-dwim nil))
       (1 (er/expand-region 1)))))
 
