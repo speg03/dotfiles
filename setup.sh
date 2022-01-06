c9e6db6fef24a480f2c77f682730363508ed3b5d
Takahiro Yano <speg03@gmail.com>
2022-01-07 01:19:40 +0900

    Relocate dotfiles

--- setup.sh
+++ setup.sh
@@ -6,15 +6,14 @@ DOTFILES_ROOT="$HOME/.local/src/github.com/speg03/dotfiles"
 if [[ ! -e $DOTFILES_ROOT ]]; then
     git clone https://github.com/speg03/dotfiles "$DOTFILES_ROOT"
 fi
-cd "$DOTFILES_ROOT"
 
 # ------------------------------
 # Symbolic links
 # ------------------------------
 
 symlink() {
-    local src_path=$1
-    local dst_path=$2
+    local src_path="$DOTFILES_ROOT/$1"
+    local dst_path=${2:-"$HOME/.$1"}  # 第2引数が省略された場合はHOMEからの相対パスを宛先にする
 
     if [[ ! -L $dst_path && -e $dst_path ]]; then
         # 宛先がシンボリックリンク以外で存在する場合
@@ -25,27 +24,29 @@ symlink() {
     fi
 }
 
+symlink local/bin/is-online
+symlink local/bin/tmux-new-or-switch-session
+symlink local/bin/tmux-plugin-manager
 
-for src in "$DOTFILES_ROOT"/config/*; do
-    symlink "$src" "$HOME/.config/$(basename "$src")"
-done
+symlink config/git/config
+symlink config/tmux/tmux.conf
+symlink config/flake8
+symlink config/starship.toml
 
-for src in "$DOTFILES_ROOT"/bin/*; do
-    symlink "$src" "$HOME/.local/bin/$(basename "$src")"
-done
-
-symlink .config/zsh/zshenv "$HOME/.zshenv"
-symlink .config/zsh/zshrc "$HOME/.zshrc"
+symlink config/zsh/zshenv "$HOME/.zshenv"
+symlink config/zsh/zshrc "$HOME/.zshrc"
 
 if [[ $(uname -s) == Darwin ]]; then
-    symlink "$HOME/.config/gnupg/gpg-agent_darwin.conf" "$HOME/.gnupg/gpg-agent.conf"
-    symlink keybindings_darwin.json "$HOME/.config/Code/User/keybindings.json"
-    symlink "$HOME/.config/Code/User" "$HOME/Library/Application Support/Code/User"
-    symlink "$HOME/.config/pypoetry" "$HOME/Library/Application Support/pypoetry"
+    symlink config/karabiner/karabiner.json
+    symlink config/gnupg/gpg-agent_darwin.conf "$HOME/.gnupg/gpg-agent.conf"
+    symlink config/vscode/settings.json "$HOME/Library/Application Support/Code/User/settings.json"
+    symlink config/vscode/keybindings_darwin.json "$HOME/Library/Application Support/Code/User/keybindings.json"
+    symlink config/pypoetry/config.toml "$HOME/Library/Application Support/pypoetry/config.toml"
 elif [[ $(uname -s) == Linux ]]; then
-    symlink "$HOME/.config/gnupg/gpg-agent_linux.conf" "$HOME/.gnupg/gpg-agent.conf"
-    symlink keybindings_linux.json "$HOME/.config/Code/User/keybindings.json"
-    symlink "$HOME/.config/Code/User" "$HOME/.vscode-server/data/Machine"
+    symlink config/gnupg/gpg-agent_linux.conf "$HOME/.gnupg/gpg-agent.conf"
+    symlink config/vscode/settings.json "$HOME/.vscode-server/data/Machine/settings.json"
+    symlink config/vscode/keybindings_linux.json "$HOME/.vscode-server/data/Machine/keybindings.json"
+    symlink config/pypoetry/config.toml
 fi
 
 # ------------------------------
